ecd149d43da1909c83e9603984ffb1bee3785c4e
Alok Sharma <aloksharma10969@gmail.com>
2024-04-04 20:46:05 +0530

    commit

--- lib/widgets/home_widgets/emergencies/FamilyEmergency.dart
+++ lib/widgets/home_widgets/emergencies/FamilyEmergency.dart
@@ -1,16 +1,145 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+// import 'package:go_secure_safe/db/db_services.dart';
+// import 'package:go_secure_safe/model/contactsm.dart';
+
+// class FamilyEmergency extends StatelessWidget {
+//   _callNumber(String number) async {
+//     await FlutterPhoneDirectCaller.callNumber(number);
+//   }
+
+//   familyContact() async {
+//     List<TContact> contactList = await DatabaseHelper().getContactList();
+//     _callNumber(contactList[0].number);
+//   }
+
+//  Future<String> getNumber() async {
+//     List<TContact> contactList = await DatabaseHelper().getContactList();
+//     if (contactList.isNotEmpty) {
+//       return contactList[0].number.toString();
+//     } else {
+//       return 'No contact available'; // Default message if no contact found
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.only(left: 10.0, bottom: 5),
+//       child: Card(
+//         elevation: 5,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(20),
+//         ),
+//         child: InkWell(
+//           onTap: () => familyContact(),
+//           child: Container(
+//             height: 180,
+//             width: MediaQuery.of(context).size.width * 0.7,
+//             decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(20),
+//                 gradient: LinearGradient(
+//                   begin: Alignment.topLeft,
+//                   end: Alignment.bottomRight,
+//                   colors: [
+//                     Color.fromARGB(255, 48, 226, 226),
+//                     Color.fromARGB(255, 61, 149, 175),
+//                     Color.fromARGB(255, 173, 183, 187),
+//                   ],
+//                 )),
+//             child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   CircleAvatar(
+//                     radius: 25,
+//                     backgroundColor: Colors.white.withOpacity(0.5),
+//                     child: Image.asset('assets/add_pic.png', width: 28,),
+//                   ),
+//                   Expanded(
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           'ICE',
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: MediaQuery.of(context).size.width * 0.06,
+//                           ),
+//                         ),
+//                         Text(
+//                           'Call To Action Family Members',
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontSize: MediaQuery.of(context).size.width * 0.045,
+//                           ),
+//                         ),
+//                         Container(
+//                           height: 30,
+//                           width: 80,
+//                           decoration: BoxDecoration(
+//                             color: Colors.white,
+//                             borderRadius: BorderRadius.circular(20),
+//                           ),
+//                           child: Center(
+//                             child: Text(
+//                               getNumber(),
+//                               style: TextStyle(
+//                                 color: Colors.red[300],
+//                                 fontWeight: FontWeight.bold,
+//                                 fontSize:
+//                                     MediaQuery.of(context).size.width * 0.055,
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:go_secure_safe/db/db_services.dart';
-import 'package:go_secure_safe/model/contactsm.dart';
+import 'package:go_secure_safe/model/contactsm.dart'; // Assuming you have a database helper class
+
+class Emergency extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Emergency'),
+      ),
+      body: Center(
+        child: FamilyEmergency(),
+      ),
+    );
+  }
+}
 
 class FamilyEmergency extends StatelessWidget {
-  _callNumber(String number) async {
+  Future<void> _callNumber(String number) async {
     await FlutterPhoneDirectCaller.callNumber(number);
   }
 
-  familyContact() async {
+  Future<String> getNumber() async {
     List<TContact> contactList = await DatabaseHelper().getContactList();
-    _callNumber(contactList[0].number);
+    if (contactList.isNotEmpty) {
+      return contactList[0].number.toString();
+    } else {
+      return 'No contact available'; // Default message if no contact found
+    }
   }
 
   @override
@@ -23,21 +152,27 @@ class FamilyEmergency extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
         ),
         child: InkWell(
-          onTap: () => familyContact(),
+          onTap: () async {
+            String number = await getNumber();
+            if (number != 'No contact available') {
+              await _callNumber(number);
+            }
+          },
           child: Container(
             height: 180,
             width: MediaQuery.of(context).size.width * 0.7,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color(0xFFFD8080),
-                    Color(0xFFFB8580),
-                    Color(0xFFFBD079),
-                  ],
-                )),
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 48, 226, 226),
+                  Color.fromARGB(255, 61, 149, 175),
+                  Color.fromARGB(255, 173, 183, 187),
+                ],
+              ),
+            ),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
@@ -46,7 +181,7 @@ class FamilyEmergency extends StatelessWidget {
                   CircleAvatar(
                     radius: 25,
                     backgroundColor: Colors.white.withOpacity(0.5),
-                    child: Image.asset('assets/ambulance.png'),
+                    child: Image.asset('assets/add_pic.png', width: 28,),
                   ),
                   Expanded(
                     child: Column(
@@ -54,7 +189,7 @@ class FamilyEmergency extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Family',
+                          'ICE',
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
@@ -68,28 +203,32 @@ class FamilyEmergency extends StatelessWidget {
                             fontSize: MediaQuery.of(context).size.width * 0.045,
                           ),
                         ),
-                        Container(
-                          height: 30,
-                          width: 80,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '1-0-2',
-                              style: TextStyle(
-                                color: Colors.red[300],
-                                fontWeight: FontWeight.bold,
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.055,
+                        FutureBuilder<String>(
+                          future: getNumber(),
+                          builder: (context, snapshot) {
+                            return Container(
+                              height: 30,
+                              width: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
                               ),
-                            ),
-                          ),
+                              child: Center(
+                                child: Text(
+                                  snapshot.data ?? '',
+                                  style: TextStyle(
+                                    color: Colors.red[300],
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: MediaQuery.of(context).size.width * 0.055,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                       ],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
